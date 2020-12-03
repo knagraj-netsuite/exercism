bf6977545eb1663d0597ba9fa2ea9cd44b3dc417
knagraj <knagraj@netsuite.com>
2020-12-03 14:09:33 -0500

    Implement Hello World, and error conditions

--- javascript/hello-world/hello-world.js
+++ javascript/hello-world/hello-world.js
@@ -3,6 +3,11 @@
 // convenience to get you started writing code faster.
 //
 
-export const hello = () => {
+export function helloWithError ()  {
   throw new Error("Remove this statement and implement this function");
-};
+}
+
+export const hello = () => {
+  return "Hello, World!";
+}
+
--- javascript/hello-world/hello-world.spec.js
+++ javascript/hello-world/hello-world.spec.js
@@ -1,7 +1,29 @@
-import { hello } from './hello-world';
+import  {hello, helloWithError}  from './hello-world';
 
 describe('Hello World', () => {
-  test('Say Hi!', () => {
+
+  test('Say Hi', () => {
     expect(hello()).toEqual('Hello, World!');
+  });
+
+  test('Error checking 1', () => {
+    // expect(abc()).toEqual('Hello, World!');
+    expect(() => {helloWithError()}).toThrow(Error);
+  });
+
+  test('Error checking', () => {
+    expect(() => {helloWithError()}).toThrowError();
+  });
+
+  test('Error checking 2', () => {
+    expect(() => {helloWithError()}).toThrowError('Remove this statement and implement this function');
+  });
+
+  test('Error checking 3', () => {
+    expect(() => {helloWithError()}).toThrowError(new Error('Remove this statement and implement this function'));
+  });
+
+  test('Error checking 4', () => {
+    expect(() => {throw new Error()}).toThrowError(new Error());
   });
 });
