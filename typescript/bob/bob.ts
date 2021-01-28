4bcb0be22148310fb0fb196e50c620202b1b612d
Jason Taylor <jason@taylorj.ca>
2021-01-28 13:53:58 -0500

    Update bob.ts

--- typescript/bob/bob.ts
+++ typescript/bob/bob.ts
@@ -2,12 +2,20 @@ class Bob {
   hey(arg1: string) : string {
     // yelling if all caps
     if (arg1.toUpperCase() === arg1) {
+      if (this.isAskingQuestion(arg1)) {
+        return "Calm down, I know what I'm doing!"
+      }
+
       return 'Whoa, chill out!';
-    } else if (arg1.charAt(arg1.length - 1) === '?') {
+    } else if (this.isAskingQuestion(arg1)) {
       return 'Sure.';
     } else {
       return 'Whatever.';
     }
+  }
+
+  private isAskingQuestion(arg1: string) {
+    return arg1.charAt(arg1.length - 1) === '?';
   }
 }
 
